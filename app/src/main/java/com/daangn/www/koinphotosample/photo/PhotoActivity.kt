5231966c99802b165288de9a10f47b170723eb57
kai <iam1492@gmail.com>
2018-07-23 09:39:56 +0900

    git ignore 수정

--- app/src/main/java/com/daangn/www/koinphotosample/photo/PhotoActivity.kt
+++ app/src/main/java/com/daangn/www/koinphotosample/photo/PhotoActivity.kt
@@ -1,11 +1,10 @@
 package com.daangn.www.koinphotosample.photo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.daangn.www.koinphotosample.R
 import com.daangn.www.koinphotosample.model.Photo
@@ -14,7 +13,6 @@ import org.koin.android.ext.android.inject
 class PhotoActivity : AppCompatActivity(), PhotoScene {
 
     private val presenter: PhotoPresenter by inject()
-    val cancelableDialog: AlertDialog.Builder by inject("cancelableDialogBuilder")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
