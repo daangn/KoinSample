96475ff73328411dba9562d4e6b48ac0814ecd8e
kai <iam1492@gmail.com>
2018-07-23 09:34:34 +0900

    First commit

---
+++ app/src/main/java/com/daangn/www/koinphotosample/photo/PhotoActivity.kt
@@ -0,0 +1,46 @@
+package com.daangn.www.koinphotosample.photo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.bumptech.glide.Glide
+import com.daangn.www.koinphotosample.R
+import com.daangn.www.koinphotosample.model.Photo
+import org.koin.android.ext.android.inject
+
+class PhotoActivity : AppCompatActivity(), PhotoScene {
+
+    private val presenter: PhotoPresenter by inject()
+    val cancelableDialog: AlertDialog.Builder by inject("cancelableDialogBuilder")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+
+        presenter.scene = this
+        presenter.requestPhoto(1)
+    }
+
+    override fun onLoadPhoto(photo: Photo) {
+        val photoView = findViewById<ImageView>(R.id.thumbnail_url)
+        val title = findViewById<TextView>(R.id.title)
+
+        title.text = photo.title
+        Glide.with(this)
+            .load(photo.thumbnailUrl)
+            .into(photoView)
+    }
+
+    override fun onLoadFail(error: String?) {
+        Toast.makeText(this, "load failed: $error", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.dropScene(this)
+    }
+}
+
